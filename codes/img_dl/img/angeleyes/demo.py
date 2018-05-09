c31639369adf89d48ef0377239bb34ee2f211950
wen-fei <282699766@qq.com>
2018-05-09 21:16:21 +0800

    config

---
+++ codes/img_dl/img/angeleyes/demo.py
@@ -0,0 +1,3 @@
+import os
+
+dir = os.listdir('sefef')
---
+++ codes/img_dl/img/angeleyes/fileConfig.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*-
+"""
+模块载入需要的文件目录
+"""
+#windows
+VGG_FACE_LOCATION = 'D://vgg.pkl'
+FEATURE_LOCATION = 'D:\\feature\\feature.dat'
+ID_LOCATION = 'D:\\feature\\id.dat'
+HAARCASCADE_EYES_LOCATION = 'D://haarcascade_eye.xml'
+HARRCASCADE_FACE_LOCATION = 'D://haarcascade_frontalface_default.xml'
+DLIB_FACE_CNN_DETECTION = 'D://mmod_human_face_detector.dat'
+DLIB_SHAPE_LOCATION = 'D://shape_predictor_5_face_landmarks.dat'
+
+#ubuntu
+# VGG_FACE_LOCATION = '/media/wall/F6F4D817F4D7D7C7/vgg.pkl'
+# FEATURE_LOCATION = '/media/wall/F6F4D817F4D7D7C7/feature/feature.dat'
+# ID_LOCATION = '/media/wall/F6F4D817F4D7D7C7/feature/id.dat'
+# HAARCASCADE_EYES_LOCATION = '/media/wall/F6F4D817F4D7D7C7/haarcascade_eye.xml'
+# HARRCASCADE_FACE_LOCATION = '/media/wall/F6F4D817F4D7D7C7/haarcascade_frontalface_default.xml'
+# DLIB_FACE_CNN_DETECTION = '/media/wall/F6F4D817F4D7D7C7/mmod_human_face_detector.dat'
+# DLIB_SHAPE_LOCATION = '/media/wall/F6F4D817F4D7D7C7/shape_predictor_5_face_landmarks.dat'
+
+#vps
+# VGG_FACE_LOCATION = '/home/zhangteng/data/resource/vgg.pkl'
+# FEATURE_LOCATION = '/home/zhangteng/data/resource/feature/feature.dat'
+# ID_LOCATION = '/home/zhangteng/data/resource/feature/id.dat'
+# HAARCASCADE_EYES_LOCATION = '/home/zhangteng/data/resource/haarcascade_eye.xml'
+# HARRCASCADE_FACE_LOCATION = '/home/zhangteng/data/resource/haarcascade_frontalface_default.xml'
+# DLIB_FACE_CNN_DETECTION = '/home/zhangteng/data/resource/mmod_human_face_detector.dat'
+# DLIB_SHAPE_LOCATION = '/home/zhangteng/data/resource/shape_predictor_5_face_landmarks.dat'
